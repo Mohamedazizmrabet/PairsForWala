17691dbd1202aaea3aae7bc8a6f1fc95504fb6e9
Mohamed Mrabet <81630459+Mohamedazizmrabet@users.noreply.github.com>
2023-10-10 15:27:07 +0100

    trying to make it more easy to run this project

--- runIt.py
+++ runIt.py
@@ -8,11 +8,15 @@ env_path = os.path.join('server', 'db', '.env')
 with open(env_path, 'w') as env_file:
     env_file.write(env_content)
 
+subprocess.run(['npm', 'i'], cwd='server')
+
 # Step 2: Run Migrations
 subprocess.run(['npx', 'prisma', 'migrate', 'dev'], cwd='server')
 
 # Step 3: Generate Prisma Client
 subprocess.run(['npx', 'prisma', 'generate'], cwd='server')
+
+subprocess.run(['npm', 'i'], cwd='my-app')
 
 # Step 4: Start Next.js Development Server
 subprocess.run(['npm', 'run', 'dev'], cwd='my-app')
